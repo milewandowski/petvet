b81ba1dad532f3fd319981256d7b5918713b5d10
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-02-05 19:02:19 +0000

    Add DataSource and Hibernate configuration

--- src/main/java/com/lewandowski/config/AppConfiguration.java
+++ src/main/java/com/lewandowski/config/AppConfiguration.java
@@ -1,16 +1,24 @@
 package com.lewandowski.config;
 
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.lewandowski")
+@EnableTransactionManagement
 public class AppConfiguration {
 
     @Bean
@@ -22,6 +30,42 @@ public class AppConfiguration {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/petvet?serverTimezone=UTC");
+        config.setUsername("crmlew");
+        config.setPassword("crmlew");
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory =
+                new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.lewandowski.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty(
+            "hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+        return hibernateProperties;
     }
 
 }
