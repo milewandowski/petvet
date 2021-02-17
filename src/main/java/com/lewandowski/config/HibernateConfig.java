c27c11f064010adc016f0a4aa92d57cf811f5fa2
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-02-17 14:28:43 +0000

    Add properties file

--- src/main/java/com/lewandowski/config/HibernateConfig.java
+++ src/main/java/com/lewandowski/config/HibernateConfig.java
@@ -6,6 +6,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,16 +17,24 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource({ "classpath:database.properties"})
 public class HibernateConfig {
+
+    private final Environment env;
+
+    @Autowired
+    public HibernateConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
 
-        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/petvet?serverTimezone=UTC");
-        config.setUsername("crmlew");
-        config.setPassword("crmlew");
+        config.setDriverClassName(env.getProperty("datasource.driverclass"));
+        config.setJdbcUrl(env.getProperty("datasource.url"));
+        config.setUsername(env.getProperty("datasource.username"));
+        config.setPassword(env.getProperty("datasource.password"));
         config.addDataSourceProperty( "cachePrepStmts" , "true" );
         config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
         config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
@@ -55,9 +65,7 @@ public class HibernateConfig {
     private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty(
-                "hibernate.hbm2ddl.auto", "none");
-        hibernateProperties.setProperty(
-                "hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                "hibernate.dialect", env.getProperty("hib.dialect"));
 
         return hibernateProperties;
     }
